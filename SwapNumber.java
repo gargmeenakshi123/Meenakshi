df9cd778ba93ce07282fd42b3e8357100dca3c2e
Meenaksh Garg <gargmeenakshi123@gmail.com>
2024-05-29 18:20:39 -0500

    initial comit

---
+++ SwapNumber.java
@@ -0,0 +1,18 @@
+
+public class SwapNumber
+{
+	public static void main(String[] args)
+	{
+		SwapNumber(3,5);
+	}
+	
+	public static void SwapNumber(int i, int j)
+	{
+		System.out.println("Before swapping, first number is " + i + " and Second Number is " + j);
+		int a = 0;
+		a = j;
+		j=i;
+		i=a;
+		System.out.println("Numbers are swapped. Now first number is " + i + " and Second Number is " + j);
+	}
+}

59c17807c0c93a87d53807b634bf37fac8bcfb6e
Meenaksh Garg <gargmeenakshi123@gmail.com>
2024-05-30 14:57:54 -0500

    multiply file

---
+++ Multiply.java
@@ -0,0 +1,15 @@
+
+public class Multiply
+{
+	public static void main(String[] args)
+	{
+		MultiplyNo(3,5);
+		System.out.println("Test");
+	}
+	
+	public static void MultiplyNo(int i, int j)
+	{
+		System.out.println("Multiplication is " + i*J);
+		
+	}
+}
